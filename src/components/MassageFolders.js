40533af1452d73ca9d84c43b1a8647689831db5c
Alex Pilugin <pilugin@gmail.com>
2017-01-25 03:21:08 +0000

    update

--- src/components/MassageFolders.js
+++ src/components/MassageFolders.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Tab, Nav, NavItem, Glyphicon} from 'react-bootstrap';
+import {Tab, Nav, NavItem, Glyphicon, Badge} from 'react-bootstrap';
 
   class MassageFolders extends Component {
     render() {
@@ -9,23 +9,23 @@ import {Tab, Nav, NavItem, Glyphicon} from 'react-bootstrap';
                     <Nav bsStyle="pills" stacked>
                     <NavItem eventKey="inbox">
                         <Glyphicon glyph="inbox" className="pad-right-5"/>
-                        <span className="hidden-xs">Inbox</span>
+                        <span className="hidden-xs">Inbox</span><Badge className="pull-right">42</Badge>
                     </NavItem>
                     <NavItem eventKey="sent">
                         <Glyphicon glyph="envelope" className="pad-right-5"/>
-                        <span className="hidden-xs">Sent</span>
+                        <span className="hidden-xs">Sent</span><Badge className="pull-right">42</Badge>
                     </NavItem>
                     <NavItem eventKey="important">
                         <Glyphicon glyph="bookmark" className="pad-right-5"/>
-                        <span className="hidden-xs">Important</span>
+                        <span className="hidden-xs">Important</span><Badge className="pull-right">0</Badge>
                     </NavItem>
                     <NavItem eventKey="draft">
                         <Glyphicon glyph="edit" className="pad-right-5"/>
-                        <span className="hidden-xs">Drafts</span>
+                        <span className="hidden-xs">Drafts</span><Badge className="pull-right">0</Badge>
                     </NavItem>
                     <NavItem eventKey="trash">
                         <Glyphicon glyph="trash" className="pad-right-5"/>
-                        <span className="hidden-xs">Trash</span>
+                        <span className="hidden-xs">Trash</span><Badge className="pull-right">0</Badge>
                     </NavItem>
                     </Nav>
                 </Tab.Container>
--- src/components/MsgSingleLine.js
+++ src/components/MsgSingleLine.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import {Panel, Glyphicon} from 'react-bootstrap';
+import {Checkbox, Glyphicon, Label, Row, Col} from 'react-bootstrap';
 
 class MsgSingleLine extends Component {
     render() {
         return (
             <div className="MsgSingleLine">
-                <div>aaa</div>
+            <Row className="show-grid row-no-padding flex-row flex-col-vcenter">
+                <Col xs={1} > 
+                    <Checkbox className="inline"/>
+                </Col>
+                <Col xs={1}> 
+                    <Glyphicon glyph="star-empty" />
+                </Col>
+                <Col xs={2}> 
+                    <div className="inline">Sender</div>
+                </Col>
+                <Col xs={4}> 
+                    <div className="inline">Subject</div>
+                </Col> 
+                <Col xs={1}> 
+                    <Label bsStyle="danger" className="visible-lg visible-xl">urgent!</Label>
+                </Col>
+                <Col xs={1}> 
+                    <Glyphicon glyph="paperclip" className="inline"/>
+                </Col>                   
+                <Col xs={1} className="pull-right"> 
+                    <div>Date</div>
+                </Col> 
+            </Row>
+            <div className="hline"></div>
             </div>
         )
     }
